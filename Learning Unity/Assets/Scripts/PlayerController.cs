007de126b0945619796e3b129abc0cfe9b1b2b7d
Dimitri von Rütte <dimitrivr@bluewin.ch>
2017-04-27 22:47:30 +0200

    New Project

---
+++ Learning Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float drag;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count = 0;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.drag = drag;
+
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count;
+
+		if (count >= 12)
+			winText.text = "You Win!";
+	}
+}
---
+++ Learning Unity/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	private Vector3 position;
+	private float speed;
+
+	void Start () {
+		position = transform.position;
+		speed = Random.value + 1f;
+	}
+
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		transform.position = position + (new Vector3 (0, 0.2f, 0) * Mathf.Sin (speed * Time.time));
+	}
+}
